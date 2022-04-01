7b990dbd6eadaed6e53e6d85e24b502fddc29e70
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-04-01 01:42:05 -0700

    made options on home page dynamically changeable #4

--- src/components/UI/CreateCardButton.js
+++ src/components/UI/CreateCardButton.js
@@ -5,13 +5,7 @@ import Button from "@mui/material/Button";
 const CreateCardButton = () => {
   const navigate = useNavigate();
   return (
-    <Button
-      variant="outlined"
-      sx={{
-        margin: "20px",
-      }}
-      onClick={() => navigate("/new")}
-    >
+    <Button variant="outlined" onClick={() => navigate("/new")}>
       Create a new card
     </Button>
   );
