8f31d86cb48d2e75bebe97ec3e182edd151574f2
Daisugi <volble124@gmail.com>
2022-04-11 22:39:07 -0700

    rename Cards directory

---
+++ src/components/Cards/FlashCard.js
@@ -0,0 +1,64 @@
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import PrimaryButton from "../UI/PrimaryButton";
+import { CardContext } from "../../context/card-context";
+
+const FlashCard = ({ id: cardId, front, back, example, comment, level }) => {
+  const [isBack, setIsBack] = useState(false);
+  const { dispatch } = useContext(CardContext);
+
+  const cardFlipHandler = () => {
+    setIsBack(!isBack);
+  };
+
+  const deleteHandler = cardId => {
+    dispatch({ type: "DELETE_CARD", payload: cardId });
+  };
+
+  return (
+    <Card sx={{ width: "80%", margin: "15px auto", padding: "15px" }}>
+      <div className="card">
+        {!isBack ? (
+          <CardContent onClick={cardFlipHandler}>
+            <Typography
+              sx={{ fontSize: 14 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              {level}
+            </Typography>
+            <Typography sx={{ fontSize: 28, mb: 1.5 }}>{front}</Typography>
+            <Typography variant="body2">{example}</Typography>
+          </CardContent>
+        ) : (
+          <CardContent onClick={cardFlipHandler}>
+            <Typography
+              sx={{ fontSize: 14 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              {level}
+            </Typography>
+            <Typography sx={{ fontSize: 28, mb: 1.5 }}>{back}</Typography>
+            <Typography variant="body2">{comment}</Typography>
+          </CardContent>
+        )}
+        <div className="center-row">
+          <Link to={`/cards/${cardId}/edit`}>
+            <PrimaryButton>EDIT</PrimaryButton>
+          </Link>
+
+          <PrimaryButton onClick={() => deleteHandler(cardId)}>
+            DELETE
+          </PrimaryButton>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default FlashCard;
