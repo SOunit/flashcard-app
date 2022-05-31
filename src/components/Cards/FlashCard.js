fa98e44c6a1820c5b36b432532aab6507ca7f0ff
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-05-31 12:17:08 -0700

    added filters by levels

--- src/components/Cards/FlashCard.js
+++ src/components/Cards/FlashCard.js
@@ -6,6 +6,7 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import PrimaryButton from "../UI/PrimaryButton";
 import { CardContext } from "../../context/card-context";
+import classes from "./FlashCard.module.css";
 
 const FlashCard = ({ id: cardId, front, back, example, comment, level }) => {
   const [isBack, setIsBack] = useState(false);
@@ -21,31 +22,35 @@ const FlashCard = ({ id: cardId, front, back, example, comment, level }) => {
 
   return (
     <Card sx={{ width: "80%", margin: "15px auto", padding: "15px" }}>
-      <div className="card">
+      <div className={classes["card-container"]} onClick={cardFlipHandler}>
         {!isBack ? (
-          <CardContent onClick={cardFlipHandler}>
-            <Typography
-              sx={{ fontSize: 14 }}
-              color="text.secondary"
-              gutterBottom
-            >
-              {level}
-            </Typography>
-            <Typography sx={{ fontSize: 28, mb: 1.5 }}>{front}</Typography>
-            <Typography variant="body2">{example}</Typography>
-          </CardContent>
+          <div className={classes.card}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                {level}
+              </Typography>
+              <Typography sx={{ fontSize: 28, mb: 1.5 }}>{front}</Typography>
+              <Typography variant="body2">{example}</Typography>
+            </CardContent>
+          </div>
         ) : (
-          <CardContent onClick={cardFlipHandler}>
-            <Typography
-              sx={{ fontSize: 14 }}
-              color="text.secondary"
-              gutterBottom
-            >
-              {level}
-            </Typography>
-            <Typography sx={{ fontSize: 28, mb: 1.5 }}>{back}</Typography>
-            <Typography variant="body2">{comment}</Typography>
-          </CardContent>
+          <div className={classes.card}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                {level}
+              </Typography>
+              <Typography sx={{ fontSize: 28, mb: 1.5 }}>{back}</Typography>
+              <Typography variant="body2">{comment}</Typography>
+            </CardContent>
+          </div>
         )}
         <div className="center-row">
           <Link to={`/cards/${cardId}/edit`}>
--- src/components/cards/CardList.js
+++ src/components/cards/CardList.js
@@ -2,10 +2,6 @@ import React from "react";
 import FlashCard from "./FlashCard";
 
 const CardList = ({ cards }) => {
-  if (!cards || cards.length === 0) {
-    return <div>No cards yet</div>;
-  }
-
   return cards && cards.map(card => <FlashCard key={card.id} {...card} />);
 };
 
